8509d27eeeeb96f7fa90970701b6b38873e385d1
natsumi minowa <apple@appurunoMacBook-Air.local>
2020-10-20 21:38:36 +0900

    first commit

---
+++ src/backend/index.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import messageController from './controllers/message'
+
+const app: express.Express = express()
+
+// 追加 サーバの強化追加
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// 追加終了
+
+// 追加 処理の外だし
+app.use("/messages", messageController)
+app.use("/", express.static(__dirname + "/public"))
+// 追加終了
+
+// 3000番ポートでAPIサーバ起動
+app.listen(3000, () => {
+    console.log('ポート3000番で起動したよ〜〜！')
+})
